a632203eeacc8914bf724808bbc0b9a51fc89953
Lucas Soares <lsoares267@gmail.com>
2016-07-01 09:12:17 -0300

    Numeros Primos
    c#

---
+++ Primos/Primos.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primos
+{
+    class Primos
+    {
+        public int limiteDaLista = 20000;
+        public int currentPosition = 0;
+
+        public List<int> numeros = new List<int>();
+
+        public void escreveLista()
+        {
+            int count = 2;
+            while (count <= limiteDaLista)
+            {
+                numeros.Add(count);
+                count++;
+            }
+        }
+
+        public void marcaDeleta() {
+            int currentValue = numeros.ElementAt(currentPosition);
+            for (int i = currentPosition + 1; i < numeros.Count; i++)
+            {
+                if (numeros.ElementAt(i) % currentValue == 0)
+                {
+                    numeros.RemoveAt(i);
+                }
+            }
+            currentPosition++;
+            if (currentPosition < numeros.Count)
+            {
+                marcaDeleta();
+            }
+        }
+
+
+        public void printNumeros()
+        {
+            foreach (var item in numeros)
+            {
+                Console.Write(item.ToString() + "; ");
+            }
+        }
+    }
+}
---
+++ Primos/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primos
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Primos p = new Primos();
+            p.limiteDaLista = int.Parse(Console.ReadLine());
+
+            p.escreveLista();
+            p.printNumeros();
+            p.marcaDeleta();
+            Console.ReadKey();
+            Console.WriteLine();
+            p.printNumeros();
+            Console.ReadKey();
+
+            //static int x;
+            //static int y;
+            //static int i;
+            //static List<string> lista = new List<string>();
+            //static void Main(string[] args)
+            //{
+
+            //    y = 3;
+
+            //    while(y <= 10)
+            //    {
+            //        for (i = 3; i <= 10; i++)
+            //        {
+            //           if(i % y != 0)
+            //           {
+            //               lista.Add(i.ToString());
+            //           }
+            //        }
+
+            //        y++;
+            //    }
+
+            //    lista.Sort();
+            //    Console.WriteLine(lista.Count);
+            //    Console.ReadKey();
+            //}
+        }
+    }
+}
